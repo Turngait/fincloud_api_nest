351f5b5850b40003bb27a291f2844df785eccd71
IliaRy <i.ryzhov@modesco.ru>
2023-12-07 21:18:32 +0400

    Add description

--- analytics/main.py
+++ analytics/main.py
@@ -1,19 +1,11 @@
 import paths
 
-# from flask import request
 from app import app
 import finance_routes
 from dotenv import dotenv_values
-import os
 
 @app.route('/')
 def index():
-      # user_agent = request.headers.get('User-Agent')
-      # is_redis_connect = redis_client.get('test')
-      # print(is_redis_connect)
-      # online_users = mongo.db.users.find()
-      # # db.create_all()
-      print(os.getenv('DB_PASS'))
       return '<h1>Hello, user</h1>'
 
 
